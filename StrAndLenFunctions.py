301370bd532b97ce7354bc98e49d399e4b3ba724
Jessica Rickey <46803471+jrickey24@users.noreply.github.com>
2020-08-15 13:01:33 -0500

    Add files via upload

---
+++ StrAndLenFunctions.py
@@ -0,0 +1,5 @@
+# len() gets the length of the string. str() allows the integer value
+# returned by the len() function to be treated as a string value
+# in order to be concatenated to the  existing string value
+
+print('Hello has ' + str(len('Hello')) + ' letters in it.')
